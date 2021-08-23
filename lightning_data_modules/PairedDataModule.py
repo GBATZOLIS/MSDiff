bef7c661176e26cad9f46c29b7e756759c501ac1
GBATZOLIS <gb511@cam.ac.uk>
2021-08-23 18:06:12 +0300

    almost ready for conditional score experiments

--- lightning_data_modules/PairedDataModule.py
+++ lightning_data_modules/PairedDataModule.py
@@ -10,10 +10,34 @@ from . import utils
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 
-IMG_EXTENSIONS = [
-    '.jpg', '.JPG', '.jpeg', '.JPEG',
-    '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP', '.webp', '.npy'
-]
+
+@utils.register_lightning_datamodule(name='paired')
+class PairedDataModule(pl.LightningDataModule):
+    def __init__(self, config, path):
+        #DataLoader arguments
+        self.config = config
+        self.train_workers = config.training.workers
+        self.val_workers = config.validation.workers
+        self.test_workers = config.eval.workers
+
+        self.train_batch = config.training.batch_size
+        self.val_batch = config.validation.batch_size
+        self.test_batch = config.eval.batch_size
+
+    def setup(self, stage=None): 
+        self.train_dataset = PairedDataset(self.config, phase='train')
+        self.val_dataset = PairedDataset(self.config, phase='val')
+        self.test_dataset = PairedDataset(self.config, phase='test')
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size = self.train_batch, shuffle=True, num_workers=self.train_workers) 
+  
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size = self.val_batch, shuffle=False, num_workers=self.val_workers) 
+  
+    def test_dataloader(self): 
+        return DataLoader(self.test_dataset, batch_size = self.test_batch, shuffle=False, num_workers=self.test_workers) 
+
 
 class PairedDataset(data.Dataset):
     """A template dataset class for you to implement custom datasets."""
@@ -140,32 +164,6 @@ class PairedDataset(data.Dataset):
         """Return the total number of images."""
         return len(self.image_paths['A'])
 
-@utils.register_lightning_datamodule(name='paired')
-class PairedDataModule(pl.LightningDataModule):
-    def __init__(self, config, path):
-        #DataLoader arguments
-        self.config = config
-        self.train_workers = config.training.workers
-        self.val_workers = config.validation.workers
-        self.test_workers = config.eval.workers
-
-        self.train_batch = config.training.batch_size
-        self.val_batch = config.validation.batch_size
-        self.test_batch = config.eval.batch_size
-
-    def setup(self, stage=None): 
-        self.train_dataset = PairedDataset(self.config, phase='train')
-        self.val_dataset = PairedDataset(self.config, phase='val')
-        self.test_dataset = PairedDataset(self.config, phase='test')
-
-    def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size = self.train_batch, shuffle=True, num_workers=self.train_workers) 
-  
-    def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size = self.val_batch, shuffle=False, num_workers=self.val_workers) 
-  
-    def test_dataloader(self): 
-        return DataLoader(self.test_dataset, batch_size = self.test_batch, shuffle=False, num_workers=self.test_workers) 
 
 
 def center_crop(img, crop_left, crop_right, crop_top, crop_bottom):
@@ -332,6 +330,10 @@ def load_image_paths(master_path, phase):
 
 
 def is_image_file(filename):
+    IMG_EXTENSIONS = [
+    '.jpg', '.JPG', '.jpeg', '.JPEG',
+    '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP', '.webp', '.npy'
+    ]
     return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
 
 
