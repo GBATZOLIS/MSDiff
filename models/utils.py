17147a6a071a05c0361827d3d49dd8e3708696dc
GBATZOLIS <gb511@cam.ac.uk>
2021-08-23 19:38:25 +0300

    almost ready for conditional score experiments

--- models/utils.py
+++ models/utils.py
@@ -95,7 +95,7 @@ def create_model(config):
   """Create the score model."""
   model_name = config.model.name
   score_model = get_model(model_name)(config)
-  score_model = score_model.to(config.device)
+  #score_model = score_model.to(config.device)
   #score_model = torch.nn.DataParallel(score_model)
   return score_model
 
