09e519522e23d9a316ce0746c83086707367d5e0
GBATZOLIS <gb511@cam.ac.uk>
2021-09-02 17:41:43 +0100

    testing

--- lightning_callbacks/PairedCallback.py
+++ lightning_callbacks/PairedCallback.py
@@ -62,7 +62,7 @@ class PairedVisualizationCallback(Callback):
 
     def on_test_batch_start(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
         y, x = batch
-        _, sampling_info = pl_module.sample(batch, show_evolution=True)
+        _, sampling_info = pl_module.sample(y, show_evolution=True) #sample x conditioned on y
         evolution = sampling_info['evolution']
         self.visualise_evolution(evolution, pl_module, batch_idx)
 
