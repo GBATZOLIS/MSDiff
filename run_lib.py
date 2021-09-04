6ea46089c3b10da2c422c91f926174b24ab34d0f
GBATZOLIS <gb511@cam.ac.uk>
2021-09-04 17:00:18 +0100

    debug test

--- run_lib.py
+++ run_lib.py
@@ -55,4 +55,6 @@ def test(config, log_path, checkpoint_path):
                           resume_from_checkpoint=checkpoint_path)
   
   trainer.fit(LightningModule, datamodule=DataModule)
-  trainer.test(LightningModule, DataModule.test_dataloader())
+
+  print(LightningModule.sigma_max_y)
+  #trainer.test(LightningModule, DataModule.test_dataloader())
