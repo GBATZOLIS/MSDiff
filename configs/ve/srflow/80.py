050073061a65d3d8169ca6cdc4a886cab2cce4eb
GBATZOLIS <gb511@cam.ac.uk>
2021-09-06 00:00:14 +0100

    debug test

--- configs/ve/srflow/80.py
+++ configs/ve/srflow/80.py
@@ -91,7 +91,7 @@ def get_config():
 
   # model
   config.model = model = ml_collections.ConfigDict()
-  model.checkpoint = None
+  model.checkpoint_path = None
   model.num_scales = 1000
   model.sigma_max_x = 300 #600 #input range is [0,1] and resolution is 64^2
   #we do not want to perturb y a lot. 
