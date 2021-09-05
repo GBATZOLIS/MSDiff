ef935dfdadf28acaab610bb83d67629de3a241f7
GBATZOLIS <gb511@cam.ac.uk>
2021-09-05 16:51:45 +0100

    debug test

--- models/ddpm.py
+++ models/ddpm.py
@@ -48,7 +48,7 @@ class DDPM(pl.LightningModule):
     dropout = config.model.dropout
     resamp_with_conv = config.model.resamp_with_conv
     self.num_resolutions = num_resolutions = len(ch_mult)
-    self.all_resolutions = all_resolutions = [config.data.image_size // (2 ** i) for i in range(num_resolutions)]
+    self.all_resolutions = all_resolutions = [config.data.image_size // (2 ** i) for i in range(num_resolutions)] #80,40,20,10
 
     AttnBlock = functools.partial(layers.AttnBlock)
     self.conditional = conditional = config.model.conditional
