d0e2a6fd45c822924f0a61713874176cd4fb99b8
GBATZOLIS <gb511@cam.ac.uk>
2021-09-06 15:28:00 +0100

    resolve dependencie

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import torch
-import tensorflow as tf
 import os
 import logging
 from matplotlib import pyplot as plt
@@ -50,32 +49,6 @@ def create_video(evolution, **kwargs):
         video_tensor = torch.stack(video_tensor)
         return video_tensor.unsqueeze(0)
 
-def restore_checkpoint(ckpt_dir, state, device):
-  if not tf.io.gfile.exists(ckpt_dir):
-    tf.io.gfile.makedirs(os.path.dirname(ckpt_dir))
-    logging.warning(f"No checkpoint found at {ckpt_dir}. "
-                    f"Returned the same state as input")
-    return state
-  else:
-    loaded_state = torch.load(ckpt_dir, map_location=device)
-    state['optimizer'].load_state_dict(loaded_state['optimizer'])
-    state['model'].load_state_dict(loaded_state['model'], strict=False)
-    state['ema'].load_state_dict(loaded_state['ema'])
-    state['step'] = loaded_state['step']
-    if 'epoch' in loaded_state.keys():
-      state['epoch'] = loaded_state['epoch']
-    return state
-
-
-def save_checkpoint(ckpt_dir, state):
-  saved_state = {
-    'optimizer': state['optimizer'].state_dict(),
-    'model': state['model'].state_dict(),
-    'ema': state['ema'].state_dict(),
-    'step': state['step'],
-    'epoch': state['epoch']
-  }
-  torch.save(saved_state, ckpt_dir)
 
 
 def compute_grad(f,x,t):
