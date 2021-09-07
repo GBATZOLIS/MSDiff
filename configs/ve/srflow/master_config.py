3187c317c61f91993073acd57589e9e2e718953e
GBATZOLIS <gb511@cam.ac.uk>
2021-09-07 14:57:22 +0100

    autoregressive sampling

--- configs/ve/srflow/master_config.py
+++ configs/ve/srflow/master_config.py
@@ -4,6 +4,6 @@ import ml_collections
 def get_config():
     config = ml_collections.ConfigDict()
     config.config_40 = config_40.get_config()
-    config.config_80 = config_80.get_config()
-    config.config_160 = config_160.get_config()
+    #config.config_80 = config_80.get_config()
+    #config.config_160 = config_160.get_config()
     return config
