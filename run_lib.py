e5e1cbdbadc4352a54778c43e1f636584b7a2021
GBATZOLIS <gb511@cam.ac.uk>
2021-09-13 11:53:20 +0300

    only 2x and 4x for debugging

--- run_lib.py
+++ run_lib.py
@@ -176,7 +176,7 @@ def multi_scale_test(master_config, log_path):
 
     previous_last_frame = 0
     new_last_frame = 0
-    for evolution in enumerate(scale_evolutions):
+    for evolution in scale_evolutions:
       new_last_frame = evolution.size(0)
       concat_video[previous_last_frame:new_last_frame, :evolution.size(1), :evolution.size(2), :evolution.size(3)]
       previous_last_frame = new_last_frame
