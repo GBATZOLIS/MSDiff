f3c71a3ab4990f2ad454ee33be9625ecc3f8d3cb
GBATZOLIS <gb511@cam.ac.uk>
2021-09-14 15:33:11 +0300

    train config 80 with 28M parameters

--- configs/ve/srflow/config_80.py
+++ configs/ve/srflow/config_80.py
@@ -60,7 +60,7 @@ def get_config():
   evaluate.workers = 4
   evaluate.begin_ckpt = 50
   evaluate.end_ckpt = 96
-  evaluate.batch_size = 25
+  evaluate.batch_size = 64
   evaluate.enable_sampling = True
   evaluate.num_samples = 50000
   evaluate.enable_loss = True
@@ -87,7 +87,7 @@ def get_config():
 
   # model
   config.model = model = ml_collections.ConfigDict()
-  model.checkpoint_path = 'experiments/beatSRFLOW/80/lightning_logs/version_2/checkpoints/epoch=160-step=230068.ckpt'
+  model.checkpoint_path = None
   model.num_scales = 1000
   model.sigma_max_x = 300 #600 #input range is [0,1] and resolution is 64^2
   #we do not want to perturb y a lot. 
@@ -113,9 +113,9 @@ def get_config():
   model.normalization = 'GroupNorm'
   model.nonlinearity = 'swish'
   model.nf = 128
-  model.ch_mult = (1, 1, 2)
+  model.ch_mult = (1, 1, 2, 2)
   model.num_res_blocks = 2
-  model.attn_resolutions = (20, 10)
+  model.attn_resolutions = (20, 10, 5)
   model.resamp_with_conv = True
   model.conditional = True
   model.conv_size = 3
