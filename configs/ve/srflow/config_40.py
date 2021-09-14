76303b6daaa50da300cea05f8526c6a2f96e8204
GBATZOLIS <gb511@cam.ac.uk>
2021-09-14 14:54:09 +0300

    renew the checkpoints for configurations 80 and 160 in srflow

--- configs/ve/srflow/config_40.py
+++ configs/ve/srflow/config_40.py
@@ -87,7 +87,7 @@ def get_config():
 
   # model
   config.model = model = ml_collections.ConfigDict()
-  model.checkpoint_path = 'experiments/beatSRFLOW/40/version_1/checkpoints/epoch=369-step=528729.ckpt'
+  model.checkpoint_path = None
   model.num_scales = 1000
   model.sigma_max_x = 300 #600 #input range is [0,1] and resolution is 64^2
   #we do not want to perturb y a lot. 
