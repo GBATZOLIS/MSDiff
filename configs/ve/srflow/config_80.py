43c2411b152c7790868fa0fb0e8e52fa2e0ffa23
GBATZOLIS <gb511@cam.ac.uk>
2021-09-17 20:03:35 +0300

    fix bug in computing stats

--- configs/ve/srflow/config_80.py
+++ configs/ve/srflow/config_80.py
@@ -29,7 +29,7 @@ def get_config():
   config.training.lightning_module = 'haar_conditional_decreasing_variance'
   config.training.batch_size = 64
   training.num_nodes = 1
-  training.gpus = 1
+  training.gpus = 2
   training.accelerator = None if training.gpus == 1 else 'ddp'
   training.accumulate_grad_batches = 1
   training.workers = 4
