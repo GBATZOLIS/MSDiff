822a386eb39166d1cc6fb168c213d02629c3c497
GBATZOLIS <gb511@cam.ac.uk>
2021-09-17 17:41:04 +0300

    fix bug in computing stats

--- run_lib.py
+++ run_lib.py
@@ -79,7 +79,6 @@ def compute_dataset_statistics(config):
   mean_save_dir = os.path.join(config.data.base_dir, 'datasets_mean', config.data.dataset+'_'+str(config.data.image_size))
   Path(mean_save_dir).mkdir(parents=True, exist_ok=True)
 
-  config.training.batch_size = 1 #make batchsize = 1
   DataModule = create_lightning_datamodule(config)
   DataModule.setup()
   train_dataloader = DataModule.train_dataloader()
