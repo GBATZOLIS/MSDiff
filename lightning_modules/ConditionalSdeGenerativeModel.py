c414c69bc5cee0aba218afeb308e30d1182c4a6a
GBATZOLIS <gb511@cam.ac.uk>
2021-09-18 04:42:09 +0300

    dataset mean incorporated in prior sampling

--- lightning_modules/ConditionalSdeGenerativeModel.py
+++ lightning_modules/ConditionalSdeGenerativeModel.py
@@ -79,7 +79,7 @@ class DecreasingVarianceConditionalSdeGenerativeModel(ConditionalSdeGenerativeMo
             self.sampling_eps = 1e-3
         elif config.training.sde.lower() == 'vesde':
             if sigma_max_y is None:
-                sigma_max_y = config.model.sigma_max_x 
+                sigma_max_y = torch.tensor(config.model.sigma_max_x)
             self.sigma_max_y = torch.tensor(sigma_max_y).float()
             sde_y = sde_lib.VESDE(sigma_min=config.model.sigma_min, sigma_max=sigma_max_y.cpu(), N=config.model.num_scales)
             
@@ -98,7 +98,7 @@ class DecreasingVarianceConditionalSdeGenerativeModel(ConditionalSdeGenerativeMo
     def reconfigure_conditioning_sde(self, config, sigma_max_y = None):
         if config.training.sde.lower() == 'vesde':
             if sigma_max_y is None:
-                sigma_max_y = config.model.sigma_max_x 
+                sigma_max_y = torch.tensor(config.model.sigma_max_x)
             self.sigma_max_y = torch.tensor(sigma_max_y).float()
             self.sde[0] = sde_lib.VESDE(sigma_min=config.model.sigma_min, sigma_max=sigma_max_y.cpu(), N=config.model.num_scales)
         else:
