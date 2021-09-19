88ed81d902813adb4ad5503440b96d5b07ef00e4
GBATZOLIS <gb511@cam.ac.uk>
2021-09-19 17:32:21 +0300

    adaptation for SR

--- configs/ve/inverse_problems/edges2shoes_conditional_likelihoodweighting.py
+++ configs/ve/inverse_problems/edges2shoes_conditional_likelihoodweighting.py
@@ -11,7 +11,7 @@ def get_config():
   config.training.lightning_module = 'conditional_decreasing_variance'
   training.batch_size = 50
   training.num_nodes = 1
-  training.gpus = 2
+  training.gpus = 1
   training.accelerator = None if training.gpus == 1 else 'ddp'
   training.accumulate_grad_batches = 1
   training.workers = 4
@@ -51,7 +51,7 @@ def get_config():
   evaluate.workers = 4
   evaluate.begin_ckpt = 50
   evaluate.end_ckpt = 96
-  evaluate.batch_size = 49
+  evaluate.batch_size = 25
   evaluate.enable_sampling = True
   evaluate.num_samples = 50000
   evaluate.enable_loss = True
