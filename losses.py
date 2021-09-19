0009b654b6da3758835a53327225489e57d94ec8
GBATZOLIS <gb511@cam.ac.uk>
2021-09-19 18:57:03 +0300

    adaptation for SR

--- losses.py
+++ losses.py
@@ -154,7 +154,7 @@ def get_inverse_problem_smld_loss_fn(sde, train, reduce_mean=False, likelihood_w
 
     if likelihood_weighting:
       losses_x=losses_x*sigmas_x[:, None, None, None]**2
-      losses_x=losses_x*sigmas_y[:, None, None, None]**2
+      losses_y=losses_y*sigmas_y[:, None, None, None]**2
       losses = torch.cat((losses_x, losses_y), dim=1)
       losses = reduce_op(losses.reshape(losses.shape[0], -1), dim=-1)
     else:
