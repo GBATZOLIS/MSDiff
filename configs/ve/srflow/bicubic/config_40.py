98f01500d52425ca29f463aae91e75ccedb477ce
GBATZOLIS <gb511@cam.ac.uk>
2021-09-20 19:44:22 +0300

    bicubic multiscale idea

--- configs/ve/srflow/bicubic/config_40.py
+++ configs/ve/srflow/bicubic/config_40.py
@@ -110,7 +110,7 @@ def get_config():
   model.embedding_type = 'fourier'
 
 
-  model.name = 'ddpm_paired'
+  model.name = 'ddpm_SR'
   model.scale_by_sigma = True
   model.ema_rate = 0.999
   model.normalization = 'GroupNorm'
