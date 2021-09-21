370e3269108e2b52c122e1ab4b8a6cf9746cedb1
GBATZOLIS <gb511@cam.ac.uk>
2021-09-21 19:15:38 +0300

    3D paired visualisation

--- configs/ve/inverse_problems/MRI_to_PET_slices3D.py
+++ configs/ve/inverse_problems/MRI_to_PET_slices3D.py
@@ -103,7 +103,7 @@ def get_config():
   model.normalization = 'GroupNorm'
   model.nonlinearity = 'swish'
   model.nf = 128
-  model.ch_mult = (1, 1, 2)
+  model.ch_mult = (1, 1, 2, 2)
   model.num_res_blocks = 2
   model.attn_resolutions = () #(24, 12, 6) -> attention is not supported for ddpm3D yet.
   model.resamp_with_conv = True
