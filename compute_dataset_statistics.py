d6825c43ff95fef287b3af918832bf28782ab1f7
GBATZOLIS <gb511@cam.ac.uk>
2021-09-26 17:09:30 +0300

    dataset info

--- compute_dataset_statistics.py
+++ compute_dataset_statistics.py
@@ -121,9 +121,6 @@ def compute_dataset_statistics(config):
       return below_threshold_vals, above_threshold_vals
     
 
-    print(min(info['max_vals']['pet']), max(info['max_vals']['pet']))
-
-    '''
     threshold = 2e5
     below_threshold_vals, above_threshold_vals = get_max_value_until_threshold(info['max_vals']['pet'], threshold)
 
@@ -136,8 +133,6 @@ def compute_dataset_statistics(config):
     print('Maximum above threshold maximum value: %.3f' % max(above_threshold_vals))
     print('Minimum above threshold maximum value: %.3f' % min(above_threshold_vals))
 
-
-    '''
 
 
 
