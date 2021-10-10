664f3d0c0459b75dc2ba705ec4d90ef395c6481f
GBATZOLIS <gb511@cam.ac.uk>
2021-10-10 03:40:10 +0100

    sequential DF2K

--- run_lib.py
+++ run_lib.py
@@ -1,4 +1,4 @@
-from models import ddpm, ncsnv2, fcn, ddpm3D #needed for model registration
+from models import ddpm, ncsnv2, fcn, ddpm3D, ncsnpp #needed for model registration
 import pytorch_lightning as pl
 import numpy as np
 
