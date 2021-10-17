b0abe4e429f074654d55af0361838e93ca954d13
GBATZOLIS <gb511@cam.ac.uk>
2021-10-17 23:44:40 +0100

    experimental extension of the code to include training with the continuous analog of the SR3 estimator

--- configs/ve/inverse_problems/image_to_image_translation/edges2shoes_ours_DV.py
+++ configs/ve/inverse_problems/image_to_image_translation/edges2shoes_ours_DV.py
@@ -9,9 +9,9 @@ def get_config():
   # training
   config.training = training = ml_collections.ConfigDict()
   config.training.lightning_module = 'conditional_decreasing_variance'
-  training.batch_size = 25
+  training.batch_size = 50
   training.num_nodes = 1
-  training.gpus = 2
+  training.gpus = 1
   training.accelerator = None if training.gpus == 1 else 'ddp'
   training.accumulate_grad_batches = 1
   training.workers = 4*training.gpus
@@ -46,7 +46,7 @@ def get_config():
   evaluate.workers = 4*training.gpus
   evaluate.begin_ckpt = 50
   evaluate.end_ckpt = 96
-  evaluate.batch_size = 25
+  evaluate.batch_size = 50
   evaluate.enable_sampling = True
   evaluate.num_samples = 50000
   evaluate.enable_loss = True
