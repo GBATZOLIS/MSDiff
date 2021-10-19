8347a21a6713dbc92ab07f1c62e4ec90eba03925
GBATZOLIS <gb511@cam.ac.uk>
2021-10-19 02:40:35 +0100

    prepare experiments

--- configs/ve/inverse_problems/inpainting/celebA_SR3.py
+++ configs/ve/inverse_problems/inpainting/celebA_SR3.py
@@ -117,8 +117,8 @@ def get_config():
   model.init_scale = 0.
   model.fourier_scale = 16
   model.conv_size = 3
-  model.input_channels = data.num_channels
-  model.output_channels = data.num_channels
+  model.input_channels = data.shape_x[0]+data.shape_y[0]
+  model.output_channels = data.shape_x[0]
 
   # optimization
   config.optim = optim = ml_collections.ConfigDict()
