a6934b1cfe5795a6e287830ef003d2a737f6668a
GBATZOLIS <gb511@cam.ac.uk>
2021-10-29 20:12:00 +0100

    fix plotting problem

--- configs/ve/inverse_problems/super-resolution/celebA_ours_DV_160.py
+++ configs/ve/inverse_problems/super-resolution/celebA_ours_DV_160.py
@@ -62,7 +62,7 @@ def get_config():
   #old settings
   evaluate.begin_ckpt = 50
   evaluate.end_ckpt = 96
-  evaluate.batch_size = 32
+  evaluate.batch_size = 16
   evaluate.enable_sampling = True
   evaluate.num_samples = 50000
   evaluate.enable_loss = True
