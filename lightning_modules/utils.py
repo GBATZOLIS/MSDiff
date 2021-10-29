c9269b8888d879279ca11ee8607891f3a61f3a96
GBATZOLIS <gb511@cam.ac.uk>
2021-10-29 16:10:04 +0100

    fix lighting errors

--- lightning_modules/utils.py
+++ lightning_modules/utils.py
@@ -24,5 +24,5 @@ def get_lightning_module_by_name(name):
 def create_lightning_module(config, checkpoint_path=None):
   lightning_module = get_lightning_module_by_name(config.training.lightning_module)(config)
   if checkpoint_path:
-    lightning_module = lightning_module.load_from_checkpoint(checkpoint_path, config)
+    lightning_module = lightning_module.load_from_checkpoint(checkpoint_path)
   return lightning_module
