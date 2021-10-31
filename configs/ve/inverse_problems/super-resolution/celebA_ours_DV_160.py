2994ea2822515b6455ee8ce45fe09ff05ad1d76b
GBATZOLIS <gb511@cam.ac.uk>
2021-10-31 02:08:13 +0000

    use path set false for debug

--- configs/ve/inverse_problems/super-resolution/celebA_ours_DV_160.py
+++ configs/ve/inverse_problems/super-resolution/celebA_ours_DV_160.py
@@ -55,7 +55,7 @@ def get_config():
   evaluate.c_steps = 'default'
   evaluate.snr = [0.1, 0.125, 0.15, 0.175, 0.2]
   evaluate.denoise = True
-  evaluate.use_path = True #new. We use a specific path of the forward diffusion of the condition instead of getting new samples from the perturbation kernel p(y_t|y_0) each time.
+  evaluate.use_path = False #new. We use a specific path of the forward diffusion of the condition instead of getting new samples from the perturbation kernel p(y_t|y_0) each time.
   evaluate.num_draws = 1
   evaluate.save_samples = True  
   evaluate.test_batch_limit = 5
