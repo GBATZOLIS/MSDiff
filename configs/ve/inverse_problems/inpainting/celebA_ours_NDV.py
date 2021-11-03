b33f990d0537849c5318d38759e7686048fabce4
GBATZOLIS <gb511@cam.ac.uk>
2021-11-03 23:40:02 +0000

    continue evaluation

--- configs/ve/inverse_problems/inpainting/celebA_ours_NDV.py
+++ configs/ve/inverse_problems/inpainting/celebA_ours_NDV.py
@@ -96,7 +96,7 @@ def get_config():
 
   # model
   config.model = model = ml_collections.ConfigDict()
-  model.checkpoint_path = None
+  model.checkpoint_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/checkpoints/inpainting/celebA-HQ-160/ours_NDV/epoch=219-step=356999.ckpt'
   model.num_scales = 1000
 
   #SIGMA INFORMATION FOR THE VE SDE
