6a89294af56b09f29ea0f385a38a097fb0f51a33
GBATZOLIS <gb511@cam.ac.uk>
2021-11-11 18:03:25 +0000

    hh

--- configs/ve/inverse_problems/inpainting/inpainting_master_config.py
+++ configs/ve/inverse_problems/inpainting/inpainting_master_config.py
@@ -3,8 +3,8 @@ import ml_collections
 
 def get_config():
     config = ml_collections.ConfigDict()
-    #config.ours_DV = celebA_ours_DV.get_config()
-    config.ours_NDV = celebA_ours_NDV.get_config()
-    #config.song = celebA_song.get_config()
-    #config.SR3 = celebA_SR3.get_config()
+    config.ours_DV = celebA_ours_DV.get_config()
+    #config.ours_NDV = celebA_ours_NDV.get_config()
+    config.song = celebA_song.get_config()
+    config.SR3 = celebA_SR3.get_config()
     return config
