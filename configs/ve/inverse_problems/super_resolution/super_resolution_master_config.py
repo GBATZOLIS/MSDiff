e5c208340806c7ea89fc6401addada37ca8e69d5
GBATZOLIS <gb511@cam.ac.uk>
2021-11-11 16:47:12 +0000

    hh

---
+++ configs/ve/inverse_problems/super_resolution/super_resolution_master_config.py
@@ -0,0 +1,11 @@
+from configs.ve.inverse_problems.super_resolution import celebA_ours_DV_160, ours_slowDV_160, celebA_ours_NDV_160, celebA_song_160, celebA_SR3_160
+import ml_collections
+
+def get_config():
+    config = ml_collections.ConfigDict()
+    config.ours_DV = celebA_ours_DV_160.get_config()
+    config.ours_slowDV = ours_slowDV_160.get_config()
+    config.ours_NDV = celebA_ours_NDV_160.get_config()
+    config.song = celebA_song_160.get_config()
+    config.SR3 = celebA_SR3_160.get_config()
+    return config
