1c7a703549f4f18d5b0d3753155db5f466e2c114
GBATZOLIS <gb511@cam.ac.uk>
2021-11-17 18:38:52 +0000

    jj

--- configs/ve/inverse_problems/super_resolution/deep_config.py
+++ configs/ve/inverse_problems/super_resolution/deep_config.py
@@ -1,8 +1,8 @@
-from configs.ve.inverse_problems.super_resolution import deep_celebA_ours_NDV_160, deep_celebA_SR3_160
+from configs.ve.inverse_problems.super_resolution import deep_celebA_ours_NDV_160, deep_celebA_SR3
 import ml_collections
 
 def get_config():
     config = ml_collections.ConfigDict()
     config.ours_NDV = deep_celebA_ours_NDV_160.get_config()
-    config.SR3 = deep_celebA_SR3_160.get_config()
+    config.SR3 = deep_celebA_SR3.get_config()
     return config
