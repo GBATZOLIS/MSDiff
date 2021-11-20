ef3e15bb8ddc1db5925fa17d2efe0f9c444cc843
GBATZOLIS <gb511@cam.ac.uk>
2021-11-20 14:49:29 +0000

    jj

--- configs/ve/inverse_problems/inpainting/interpolation/8.py
+++ configs/ve/inverse_problems/inpainting/interpolation/8.py
@@ -9,7 +9,7 @@ def get_config():
   # training
   config.training = training = ml_collections.ConfigDict()
   config.training.lightning_module = 'conditional'
-  training.conditioning_approach = 'ours_NDV'
+  training.conditioning_approach = '8'
   training.batch_size = 100
   training.num_nodes = 1
   training.gpus = 1
