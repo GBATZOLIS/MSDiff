67090639c1e23d9ab2e9c00e0bceda3cb2a5d72c
GBATZOLIS <gb511@cam.ac.uk>
2021-11-20 16:20:07 +0000

    jj

--- configs/ve/inverse_problems/image_to_image_translation/master_config.py
+++ configs/ve/inverse_problems/image_to_image_translation/master_config.py
@@ -4,7 +4,7 @@ import ml_collections
 def get_config():
     config = ml_collections.ConfigDict()
     config.ours_DV = edges2shoes_ours_DV.get_config()
-    config.ours_NDV = edges2shoes_ours_NDV.get_config()
+    #config.ours_NDV = edges2shoes_ours_NDV.get_config()
     #config.song = edges2shoes_song.get_config()
     #config.SR3 = edges2shoes_SR3.get_config()
     return config
