e968146cb20c7188b01cd4b77c28dffd3f90db02
GBATZOLIS <gb511@cam.ac.uk>
2021-11-21 21:07:38 +0000

    jj

--- configs/ve/inverse_problems/inpainting/master_config.py
+++ configs/ve/inverse_problems/inpainting/master_config.py
@@ -5,6 +5,6 @@ def get_config():
     config = ml_collections.ConfigDict()
     config.ours_DV = celebA_ours_DV.get_config()
     config.ours_NDV = celebA_ours_NDV.get_config()
-    #config.song = celebA_song.get_config()
-    #config.SR3 = celebA_SR3.get_config()
+    config.song = celebA_song.get_config()
+    config.SR3 = celebA_SR3.get_config()
     return config
