be2deb205fdf4a7a5b176d066a4b8957e85903e8
GBATZOLIS <gb511@cam.ac.uk>
2021-11-21 16:51:41 +0000

    jj

--- evaluation.py
+++ evaluation.py
@@ -303,6 +303,9 @@ def run_evaluation_pipeline(task, base_path, snr, device):
                    'samples': {}}
 
     for i, info in tqdm(enumerate(dataloader)):
+        if i>=2500:
+            break
+        
         y, x = info['y'], info['x']
         samples = info['samples']
 
