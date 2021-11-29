59cff7be19a0828b562b342b85095523b8cb6b8c
GBATZOLIS <gb511@cam.ac.uk>
2021-11-29 16:23:13 +0000

    continue

--- configs/ve/inverse_problems/MRI_to_PET/mri_to_pet_SR3.py
+++ configs/ve/inverse_problems/MRI_to_PET/mri_to_pet_SR3.py
@@ -55,7 +55,7 @@ def get_config():
 
   # data
   config.data = data = ml_collections.ConfigDict()
-  data.base_dir = 'datasets'
+  data.base_dir = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/datasets'
   data.dataset = 'ADNI'
   data.use_data_mean = False
   data.datamodule = 'DUAL-GLOW'
