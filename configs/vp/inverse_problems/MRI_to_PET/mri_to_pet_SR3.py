c12f9be2e07a3107eeb016319576968bd7760b1b
GBATZOLIS <gb511@cam.ac.uk>
2021-12-02 00:56:55 +0000

    corrections

--- configs/vp/inverse_problems/MRI_to_PET/mri_to_pet_SR3.py
+++ configs/vp/inverse_problems/MRI_to_PET/mri_to_pet_SR3.py
@@ -7,7 +7,7 @@ def get_config():
   config = ml_collections.ConfigDict()
 
   #logging
-  config.base_log_path = None #'/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/mri_to_pet'
+  config.base_log_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/mri_to_pet'
   config.experiment_name = 'vp_da++'
 
   # training
@@ -59,7 +59,7 @@ def get_config():
 
   # data
   config.data = data = ml_collections.ConfigDict()
-  data.base_dir = 'datasets' #'/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/datasets'
+  data.base_dir = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/datasets'
   data.dataset = 'ADNI'
   data.use_data_mean = False
   data.datamodule = 'DUAL-GLOW'
