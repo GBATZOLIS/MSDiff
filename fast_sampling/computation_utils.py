4148105c92a96e50cd68699a07bd3d3880c74829
GBATZOLIS <gb511@cam.ac.uk>
2021-12-07 17:25:10 +0200

    almost ready to run on the VE SDE experiment

--- fast_sampling/computation_utils.py
+++ fast_sampling/computation_utils.py
@@ -118,7 +118,7 @@ def fast_sampling_scheme(config, save_dir):
     lmodule.eval()
     lmodule.configure_sde(config)
 
-    device = 'cpu'
+    device = 'cuda:0'
 
     dsteps = 1000
     model = lmodule.score_model.to(device)
