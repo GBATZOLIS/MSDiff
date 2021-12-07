a1b86a06cf388906659f464e38a186ab427fbc43
GBATZOLIS <gb511@cam.ac.uk>
2021-12-07 18:01:06 +0200

    almost ready to run on the VE SDE experiment

--- configs/ve/unconditional/celebA-HQ-64.py
+++ configs/ve/unconditional/celebA-HQ-64.py
@@ -13,7 +13,7 @@ def get_config():
   # training
   config.training = training = ml_collections.ConfigDict()
   config.training.lightning_module = 'base'
-  training.batch_size = 64
+  training.batch_size = 128
   training.num_nodes = 1
   training.gpus = 1
   training.accelerator = None if training.gpus == 1 else 'ddp'
@@ -45,7 +45,7 @@ def get_config():
   # evaluation (this file is not modified at all - subject to change)
   config.eval = evaluate = ml_collections.ConfigDict()
   evaluate.workers = 4*training.gpus
-  evaluate.batch_size = 64
+  evaluate.batch_size = 128
   evaluate.enable_sampling = True
   evaluate.num_samples = 50000
   evaluate.enable_loss = True
