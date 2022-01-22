2b017d2dca2a39dc62e7c8b6d483965aeb190734
GBATZOLIS <gb511@cam.ac.uk>
2022-01-22 12:31:48 +0000

    fix tiny error

--- configs/ve/unconditional/celebA-HQ-64.py
+++ configs/ve/unconditional/celebA-HQ-64.py
@@ -13,7 +13,7 @@ def get_config():
   # training
   config.training = training = ml_collections.ConfigDict()
   config.training.lightning_module = 'base'
-  training.batch_size = 128
+  training.batch_size = 50
   training.num_nodes = 1
   training.gpus = 1
   training.accelerator = None if training.gpus == 1 else 'ddp'
