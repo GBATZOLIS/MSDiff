1998a9563b6d91104d5a4cce55c6112fac731bef
GBATZOLIS <gb511@cam.ac.uk>
2022-01-24 17:20:22 +0000

    sampling adaptively using the LIPSCHITZ CONSTANT and ddim solver

--- lightning_callbacks/callbacks.py
+++ lightning_callbacks/callbacks.py
@@ -243,9 +243,10 @@ class ImageVisualizationCallback(Callback):
     def on_test_batch_start(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
         if batch_idx == 0:
             for start_T in self.starting_T:
-                    for alpha in self.alpha:
-                        for adaptive in self.adaptive:
-                            self.generate_lipschitz_synthetic_dataset(pl_module, adaptive, alpha, start_T)
+                print(start_T)
+                for alpha in self.alpha:
+                    for adaptive in self.adaptive:
+                        self.generate_lipschitz_synthetic_dataset(pl_module, adaptive, alpha, start_T)
 
 
     def on_validation_epoch_end(self, trainer, pl_module):
