4567f3ebb891dbb601cc16189338b8d1bcc1dbab
GBATZOLIS <gb511@cam.ac.uk>
2022-01-25 00:10:43 +0000

    sampling adaptively using the LIPSCHITZ CONSTANT and ddim solver

--- configs/ve/unconditional/celebA-HQ-64.py
+++ configs/ve/unconditional/celebA-HQ-64.py
@@ -7,7 +7,7 @@ def get_config():
   config = ml_collections.ConfigDict()
 
   #logging
-  config.base_log_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/fast_reverse_diffusion/celebA-HQ-160/ve' #'/home/gb511/score_sde_pytorch-1/ve_fast_sampling' 
+  config.base_log_path = '/home/gb511/score_sde_pytorch-1/ve_fast_sampling' #'/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/fast_reverse_diffusion/celebA-HQ-160/ve' 
   config.experiment_name = 've_celebAHQ_64'
 
   # training
@@ -69,7 +69,7 @@ def get_config():
 
   # data
   config.data = data = ml_collections.ConfigDict()
-  data.base_dir =  '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/datasets' #'datasets'
+  data.base_dir =  'datasets' #'/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/datasets' 
   data.dataset = 'celebA-HQ-160'
   data.use_data_mean = False
   data.datamodule = 'unpaired_PKLDataset'
@@ -86,7 +86,7 @@ def get_config():
 
   # model
   config.model = model = ml_collections.ConfigDict()
-  model.checkpoint_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/fast_reverse_diffusion/celebA-HQ-160/ve/ve_celebAHQ_64/version_0/checkpoints/epoch=233-step=595295.ckpt'  #'/home/gb511/saved_checkpoints/fast_sampling/ve/celebA-HQ/64/epoch=233-step=595295.ckpt' 
+  model.checkpoint_path = '/home/gb511/saved_checkpoints/fast_sampling/ve/celebA-HQ/64/epoch=233-step=595295.ckpt'  #'/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/fast_reverse_diffusion/celebA-HQ-160/ve/ve_celebAHQ_64/version_0/checkpoints/epoch=233-step=595295.ckpt'  
   model.num_scales = 1000
   model.sigma_max = np.sqrt(np.prod(data.shape))
   model.sigma_min = 0.01
