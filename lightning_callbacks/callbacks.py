f80c58ec4912883776482b3ba05abfcc6e134792
GBATZOLIS <gb511@cam.ac.uk>
2022-02-03 19:38:31 +0000

    multiscale unconditional generation

--- lightning_callbacks/callbacks.py
+++ lightning_callbacks/callbacks.py
@@ -146,18 +146,28 @@ class MultiscaleImageVisualizationCallback(Callback):
     def on_validation_epoch_end(self, trainer, pl_module):
         current_epoch = pl_module.current_epoch
         if current_epoch !=0 and current_epoch % 5 == 0:
-            samples, sampling_info = pl_module.sample(p_steps=1000//pl_module.num_scales)
-            self.visualise_samples(samples, pl_module)
+            psteps_per_scale = 1000//pl_module.num_scales
+            samples, sampling_info = pl_module.sample(p_steps=psteps_per_scale)
+            self.visualise_samples(samples, pl_module, steps_per_scale=psteps_per_scale)
 
             #log sampling times for each scale
             for scale_name in sampling_info.keys():
-                pl_module.logger.experiment.add_scalar('sampling_time_for_scale_%s' %scale_name, sampling_info[scale_name]['time'], current_epoch)
+                pl_module.logger.experiment.add_scalar('sampling_time_for_scale_%s' % scale_name, sampling_info[scale_name]['time'], current_epoch)
 
-    def visualise_samples(self, samples, pl_module):
+            psteps_per_scale = 2000//pl_module.num_scales
+            samples, sampling_info = pl_module.sample(p_steps=psteps_per_scale)
+            self.visualise_samples(samples, pl_module, steps_per_scale=psteps_per_scale)
+
+            psteps_per_scale = 4000//pl_module.num_scales
+            samples, sampling_info = pl_module.sample(p_steps=psteps_per_scale)
+            self.visualise_samples(samples, pl_module, steps_per_scale=psteps_per_scale)
+
+            
+    def visualise_samples(self, samples, pl_module, steps_per_scale):
         # log sampled images
         sample_imgs =  samples.cpu()
         grid_images = torchvision.utils.make_grid(sample_imgs, normalize=True, scale_each=True)
-        pl_module.logger.experiment.add_image('generated_samples_%d' % pl_module.global_step, grid_images, pl_module.current_epoch)
+        pl_module.logger.experiment.add_image('generated_samples_%d_psteps_per_scale_%d' % (pl_module.global_step, steps_per_scale), grid_images, pl_module.current_epoch)
     
 
 @utils.register_callback(name='base')
