76ce5d13d12a3fc6c0a0246b1107ad6b065a3e30
GBATZOLIS <gb511@cam.ac.uk>
2022-02-03 13:10:59 +0000

    multiscale unconditional generation

--- lightning_modules/MultiScaleSdeGenerativeModel.py
+++ lightning_modules/MultiScaleSdeGenerativeModel.py
@@ -170,7 +170,7 @@ class MultiScaleSdeGenerativeModel(pl.LightningModule):
                 gamma=0., alpha=1., 
                 starting_T='default', ending_T='default'):
         
-        scale_sampling_order = self.get_scale_order(direction='reverse')
+        scale_sampling_order = self.get_scale_order()
         print(scale_sampling_order)
         x=None
         aggregate_sampling_information = {}
