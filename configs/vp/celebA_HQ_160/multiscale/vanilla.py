8805800099f94b3c3c8185ec8a2c64033fa66d4c
GBATZOLIS <gb511@cam.ac.uk>
2022-02-19 21:51:55 +0000

    resume multiscale training to reach 2M iterations

--- configs/vp/celebA_HQ_160/multiscale/vanilla.py
+++ configs/vp/celebA_HQ_160/multiscale/vanilla.py
@@ -34,7 +34,7 @@ def get_config():
   # sampling
   config.sampling = sampling = ml_collections.ConfigDict()
   sampling.method = 'pc'
-  sampling.predictor = 'ddim'
+  sampling.predictor = 'euler_maruyama'
   sampling.corrector = 'none'
   sampling.n_steps_each = 1
   sampling.noise_removal = True
