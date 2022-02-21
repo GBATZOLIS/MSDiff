f9cac9e8796e407b6fe0d56207a0b0f042055aca
GBATZOLIS <gb511@cam.ac.uk>
2022-02-21 00:52:10 +0000

    experiment with log-concave snr profile - correct snr limits - different optimisation algorithms

--- configs/vp/celebA_HQ_160/multiscale/vanilla_128_optim1.py
+++ configs/vp/celebA_HQ_160/multiscale/vanilla_128_optim1.py
@@ -84,7 +84,7 @@ def get_config():
 
   # model
   config.model = model = ml_collections.ConfigDict()
-  model.checkpoint_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/fast_reverse_diffusion/multiscale/debug_vanilla/loglinear_128/version_1/checkpoints/epoch=124-step=317999.ckpt'
+  model.checkpoint_path = None
   model.num_scales = 1000
   model.sigma_max = np.sqrt(np.prod(data.shape))
   model.sigma_min = 0.01
