b22ada9f58a25d218fa6ee1555a4f850c844b43a
GBATZOLIS <gb511@cam.ac.uk>
2022-02-24 16:51:01 +0000

    resume beta-linear and log-concave simple objective training

--- configs/vp/celebA_HQ_160/multiscale/vanilla_logconcave_simple.py
+++ configs/vp/celebA_HQ_160/multiscale/vanilla_logconcave_simple.py
@@ -84,7 +84,7 @@ def get_config():
 
   # model
   config.model = model = ml_collections.ConfigDict()
-  model.checkpoint_path = None
+  model.checkpoint_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/fast_reverse_diffusion/multiscale/debug_vanilla/128/logconcave_optim1_simple/version_0/checkpoints/epoch=85-step=218783.ckpt'
   model.num_scales = 1000
   model.sigma_max = np.sqrt(np.prod(data.shape))
   model.sigma_min = 0.01
