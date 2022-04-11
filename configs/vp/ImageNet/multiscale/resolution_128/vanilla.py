ee6924e8a57274c96ee420d38ed734d9ed807ce3
GBATZOLIS <gb511@cam.ac.uk>
2022-04-11 19:32:35 +0300

    find the maximum batchsize

--- configs/vp/ImageNet/multiscale/resolution_128/vanilla.py
+++ configs/vp/ImageNet/multiscale/resolution_128/vanilla.py
@@ -114,7 +114,7 @@ def get_config():
   
   # model architecture
   model.name = 'guided_diffusion_UNET'
-  model.model_channels = 160
+  model.model_channels = 128
   model.input_channels = data.num_channels
   model.output_channels = data.num_channels
   model.num_res_blocks = 2
