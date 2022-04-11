57de58b63a0bd6664b68ad6aef96b1e0ee43bd6a
GBATZOLIS <gb511@cam.ac.uk>
2022-04-11 20:51:15 +0300

    find the maximum batchsize

--- configs/vp/ImageNet/multiscale/resolution_128/a3.py
+++ configs/vp/ImageNet/multiscale/resolution_128/a3.py
@@ -111,7 +111,7 @@ def get_config():
   model.input_channels = data.num_channels
   model.output_channels = data.num_channels
   model.num_res_blocks = 2
-  model.attention_resolutions = (16, 8)
+  model.attention_resolutions = () #(16, 8)
   model.dropout = 0.
   model.channel_mult =  (1, 2)
   model.conv_resample = True
