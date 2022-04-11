9dc8da47c8d7a33ab71598d751011cf9eda4d05e
GBATZOLIS <gb511@cam.ac.uk>
2022-04-11 15:39:41 +0300

    find the maximum batchsize

--- configs/vp/ImageNet/multiscale/128/vanilla.py
+++ configs/vp/ImageNet/multiscale/128/vanilla.py
@@ -21,7 +21,7 @@ def get_config():
   config.training.lightning_module = 'base'
   training.num_nodes = 1
   training.gpus = 2
-  training.batch_size = 128 // (training.num_nodes*training.gpus)
+  training.batch_size = 64 // (training.num_nodes*training.gpus)
   training.accelerator = None if training.gpus == 1 else 'ddp'
   training.accumulate_grad_batches = 1
   training.workers = 4*training.gpus
