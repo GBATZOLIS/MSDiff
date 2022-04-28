c5978442399a9302847f64bfaa1ed3e2e3ec3b41
GBATZOLIS <gb511@cam.ac.uk>
2022-04-28 16:32:09 +0300

    train multiscale ++ deeper same level as vanilla

--- configs/vp/ImageNet/multiscale/resolution_128/ema/deep_multiscale/d1.py
+++ configs/vp/ImageNet/multiscale/resolution_128/ema/deep_multiscale/d1.py
@@ -14,7 +14,7 @@ def get_config():
   elif server == 'abg':
     config.base_log_path = '/home/gb511/projects/fast_sampling/ImageNet/%d' % image_size
 
-  config.experiment_name = 'multiscale_ema'
+  config.experiment_name = 'multiscale_ema_deep'
 
   # training
   config.training = training = ml_collections.ConfigDict()
@@ -122,7 +122,7 @@ def get_config():
   target = np.exp(-1/4*(20-0.1)-1/2*0.1)
   model.beta_max = (1-2/model.T_k)*model.beta_min -4/model.T_k**2 * np.log(target/2**(data.scale_depth-1))
   
-  model.checkpoint_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/fast_reverse_diffusion/multiscale/ImageNet/128/multiscale_ema/checkpoint_resuming/latest-step=1919999.ckpt'
+  model.checkpoint_path = None
   model.num_scales = 1000
   model.sigma_max = np.sqrt(np.prod(data.shape))
   model.sigma_min = 0.01
