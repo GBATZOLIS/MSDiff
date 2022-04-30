a72e1a616ff899a1a76630a828bda54338ca6c8a
GBATZOLIS <gb511@cam.ac.uk>
2022-04-30 15:21:21 +0300

    train multiscale ++ deeper same level as vanilla

--- configs/vp/ImageNet/multiscale/resolution_128/ema/a3.py
+++ configs/vp/ImageNet/multiscale/resolution_128/ema/a3.py
@@ -14,7 +14,7 @@ def get_config():
   config.training = training = ml_collections.ConfigDict()
   config.training.lightning_module = 'multiscale_base'
   training.num_nodes = 1
-  training.gpus = 2
+  training.gpus = 1
   training.batch_size = 64 // (training.num_nodes*training.gpus)
   training.accelerator = None if training.gpus == 1 else 'ddp'
   training.accumulate_grad_batches = 1
