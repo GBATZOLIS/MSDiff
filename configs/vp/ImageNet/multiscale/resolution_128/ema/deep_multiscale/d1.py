c035acd613167cba1d10bb64a9b4233c53e94557
GBATZOLIS <gb511@cam.ac.uk>
2022-05-24 16:21:54 +0100

    teaser creation for multi-scale diffusion models

--- configs/vp/ImageNet/multiscale/resolution_128/ema/deep_multiscale/d1.py
+++ configs/vp/ImageNet/multiscale/resolution_128/ema/deep_multiscale/d1.py
@@ -27,7 +27,7 @@ def get_config():
   training.accumulate_grad_batches = 1
   training.workers = 4*training.gpus
   training.num_epochs = 10000
-  training.n_iters = 2000000
+  training.n_iters = 2530000
   training.visualization_callback = 'multiscale_base'
   training.show_evolution = False
   training.use_ema = True
@@ -126,7 +126,7 @@ def get_config():
   target = np.exp(-1/4*(20-0.1)-1/2*0.1)
   model.beta_max = (1-2/model.T_k)*model.beta_min -4/model.T_k**2 * np.log(target/2**(data.scale_depth-1))
   
-  model.checkpoint_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/fast_reverse_diffusion/multiscale/ImageNet/128/multiscale_ema_deep/checkpoint_resuming/latest-step=1664999.ckpt'
+  model.checkpoint_path = '/home/gb511/rds/rds-t2-cs138-LlrDsbHU5UM/gb511/projects/fast_reverse_diffusion/multiscale/ImageNet/128/multiscale_ema_deep/checkpoint_resuming/latest-step=1999999.ckpt'
   model.num_scales = 1000
   model.sigma_max = np.sqrt(np.prod(data.shape))
   model.sigma_min = 0.01
